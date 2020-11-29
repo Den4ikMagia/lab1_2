193b0ba2ca1cf82d0e1f8e68fbe74da77ba4bc03
Denis <tim1996denis@gmail.com>
2020-11-29 14:39:26 +0300

    Доработаны задания с 7-го по 11

--- task/07-yield-tasks.js
+++ task/07-yield-tasks.js
@@ -1,12 +1,11 @@
-'use strict';
 
-/********************************************************************************************
+/** ******************************************************************************************
  *                                                                                          *
  * Plese read the following tutorial before implementing tasks:                             *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators   *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/yield        *
  *                                                                                          *
- ********************************************************************************************/
+ ******************************************************************************************* */
 
 
 /**
@@ -33,107 +32,166 @@
  *
  */
 function* get99BottlesOfBeer() {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the Fibonacci sequence:
- *   0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
- *
- * See more at: https://en.wikipedia.org/wiki/Fibonacci_number
- *
- * @return {Iterable.<number>}
- *
- */
-function* getFibonacciSequence() {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Traverses a tree using the depth-first strategy
- * See details: https://en.wikipedia.org/wiki/Depth-first_search
- *
- * Each node have child nodes in node.children array.
- * The leaf nodes do not have 'children' property.
- *
- * @params {object} root the tree root
- * @return {Iterable.<object>} the sequence of all tree nodes in depth-first order
- * @example
- *
- *   var node1 = { n:1 }, node2 = { n:2 }, node3 = { n:3 }, node4 = { n:4 },
- *       node5 = { n:5 }, node6 = { n:6 }, node7 = { n:7 }, node8 = { n:8 };
- *   node1.children = [ node2, node6, node7 ];
- *   node2.children = [ node3, node4 ];
- *   node4.children = [ node5 ];
- *   node7.children = [ node8 ];
- *
- *     source tree (root = 1):
- *            1
- *          / | \
- *         2  6  7
- *        / \     \            =>    { 1, 2, 3, 4, 5, 6, 7, 8 }
- *       3   4     8
- *           |
- *           5
- *
- *  depthTraversalTree(node1) => node1, node2, node3, node4, node5, node6, node7, node8
- *
- */
-function* depthTraversalTree(root) {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Traverses a tree using the breadth-first strategy
- * See details: https://en.wikipedia.org/wiki/Breadth-first_search
- *
- * Each node have child nodes in node.children array.
- * The leaf nodes do not have 'children' property.
- *
- * @params {object} root the tree root
- * @return {Iterable.<object>} the sequence of all tree nodes in breadth-first order
- * @example
- *     source tree (root = 1):
- *
- *            1
- *          / | \
- *         2  3  4
- *        / \     \            =>    { 1, 2, 3, 4, 5, 6, 7, 8 }
- *       5   6     7
- *           |
- *           8
- *
- */
-function* breadthTraversalTree(root) {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Merges two yield-style sorted sequences into the one sorted sequence.
- * The result sequence consists of sorted items from source iterators.
- *
- * @params {Iterable.<number>} source1
- * @params {Iterable.<number>} source2
- * @return {Iterable.<number>} the merged sorted sequence
- *
- * @example
- *   [ 1, 3, 5, ... ], [2, 4, 6, ... ]  => [ 1, 2, 3, 4, 5, 6, ... ]
- *   [ 0 ], [ 2, 4, 6, ... ]  => [ 0, 2, 4, 6, ... ]
- *   [ 1, 3, 5, ... ], [ -1 ] => [ -1, 1, 3, 5, ...]
- */
-function* mergeSortedSequences(source1, source2) {
-    throw new Error('Not implemented');
-}
-
-
-module.exports = {
-    get99BottlesOfBeer: get99BottlesOfBeer,
-    getFibonacciSequence: getFibonacciSequence,
-    depthTraversalTree: depthTraversalTree,
-    breadthTraversalTree: breadthTraversalTree,
-    mergeSortedSequences: mergeSortedSequences
-};
+    for (let bottles = 99; bottles > 2; bottles--) {
+      yield String(bottles).concat(' bottles of beer on the wall, ', bottles, ' bottles of beer.');
+      yield 'Take one down and pass it around, '.concat(bottles - 1, ' bottles of beer on the wall.');
+    }
+    yield '2 bottles of beer on the wall, 2 bottles of beer.';
+    yield 'Take one down and pass it around, 1 bottle of beer on the wall.';
+    yield '1 bottle of beer on the wall, 1 bottle of beer.';
+    yield 'Take one down and pass it around, no more bottles of beer on the wall.';
+    yield 'No more bottles of beer on the wall, no more bottles of beer.';
+    yield 'Go to the store and buy some more, 99 bottles of beer on the wall.';
+  }
+  
+  
+  /**
+   * Returns the Fibonacci sequence:
+   *   0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+   *
+   * See more at: https://en.wikipedia.org/wiki/Fibonacci_number
+   *
+   * @return {Iterable.<number>}
+   *
+   */
+  function* getFibonacciSequence() {
+    let fnF = 0;
+    let fnS = 1;
+    while (true) {
+      const current = fnF;
+      fnF = fnS;
+      fnS = current + fnF;
+      const reset = yield current;
+      if (reset) {
+        fnF = 0;
+        fnS = 1;
+      }
+    }
+  }
+  
+  
+  /**
+   * Traverses a tree using the depth-first strategy
+   * See details: https://en.wikipedia.org/wiki/Depth-first_search
+   *
+   * Each node have child nodes in node.children array.
+   * The leaf nodes do not have 'children' property.
+   *
+   * @params {object} root the tree root
+   * @return {Iterable.<object>} the sequence of all tree nodes in depth-first order
+   * @example
+   *
+   *   let node1 = { n:1 }, node2 = { n:2 }, node3 = { n:3 }, node4 = { n:4 },
+   *       node5 = { n:5 }, node6 = { n:6 }, node7 = { n:7 }, node8 = { n:8 };
+   *   node1.children = [ node2, node6, node7 ];
+   *   node2.children = [ node3, node4 ];
+   *   node4.children = [ node5 ];
+   *   node7.children = [ node8 ];
+   *
+   *     source tree (root = 1):
+   *            1
+   *          / | \
+   *         2  6  7
+   *        / \     \            =>    { 1, 2, 3, 4, 5, 6, 7, 8 }
+   *       3   4     8
+   *           |
+   *           5
+   *
+   *  depthTraversalTree(node1) => node1, node2, node3, node4, node5, node6, node7, node8
+   *
+   */
+  function* depthTraversalTree(root) {
+    const nodes = [root];
+    for (let i = 0; i < nodes.length; i++) {
+      yield nodes[i];
+      if ('children' in nodes[i]) {
+        for (let j = 0; j < nodes[i].children.length; j++) {
+          nodes.splice(i + 1 + j, 0, nodes[i].children[j]);
+        }
+      }
+    }
+  }
+  
+  
+  /**
+   * Traverses a tree using the breadth-first strategy
+   * See details: https://en.wikipedia.org/wiki/Breadth-first_search
+   *
+   * Each node have child nodes in node.children array.
+   * The leaf nodes do not have 'children' property.
+   *
+   * @params {object} root the tree root
+   * @return {Iterable.<object>} the sequence of all tree nodes in breadth-first order
+   * @example
+   *     source tree (root = 1):
+   *
+   *            1
+   *          / | \
+   *         2  3  4
+   *        / \     \            =>    { 1, 2, 3, 4, 5, 6, 7, 8 }
+   *       5   6     7
+   *           |
+   *           8
+   *
+   */
+  function* breadthTraversalTree(root) {
+    const nodes = [root];
+    for (let i = 0; i < nodes.length; i++) {
+      yield nodes[i];
+      if ('children' in nodes[i]) {
+        for (let j = 0; j < nodes[i].children.length; j++) {
+          nodes.push(nodes[i].children[j]);
+        }
+      }
+    }
+  }
+  
+  
+  /**
+   * Merges two yield-style sorted sequences into the one sorted sequence.
+   * The result sequence consists of sorted items from source iterators.
+   *
+   * @params {Iterable.<number>} source1
+   * @params {Iterable.<number>} source2
+   * @return {Iterable.<number>} the merged sorted sequence
+   *
+   * @example
+   *   [ 1, 3, 5, ... ], [2, 4, 6, ... ]  => [ 1, 2, 3, 4, 5, 6, ... ]
+   *   [ 0 ], [ 2, 4, 6, ... ]  => [ 0, 2, 4, 6, ... ]
+   *   [ 1, 3, 5, ... ], [ -1 ] => [ -1, 1, 3, 5, ...]
+   */
+  function* mergeSortedSequences(source1, source2) {
+    let src1; let
+      src2;
+    src1 = source1();
+    src2 = source2();
+    let i; let
+      j;
+    i = src1.next().value;
+    j = src2.next().value;
+    while (true) {
+      if ((i === undefined) && (j !== undefined)) {
+        yield j;
+        j = src2.next().value;
+      } else if ((j === undefined) && (i !== undefined)) {
+        yield i;
+        i = src1.next().value;
+      } else if ((i < j) && (i !== undefined) && (j !== undefined)) {
+        yield i;
+        i = src1.next().value;
+      } else if ((i >= j) && (i !== undefined) && (j !== undefined)) {
+        yield j;
+        j = src2.next().value;
+      }
+    }
+  }
+  
+  
+  module.exports = {
+    get99BottlesOfBeer,
+    getFibonacciSequence,
+    depthTraversalTree,
+    breadthTraversalTree,
+    mergeSortedSequences,
+  };
+  
